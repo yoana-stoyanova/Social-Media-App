56e1c91be726ee647059e592933fc128c759903d
Yoana Stoyanova <jstoyanova14.05@gmail.com>
2026-05-06 16:24:57 +0300

    Add project files.

---
+++ Social Media App/Comment.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+
+class Comment {
+	std::string id;
+
+	std::string userId;
+	std::string postId;
+
+	std::string content;
+};
---
+++ Social Media App/ThumbsUp.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+
+class ThumbsUp {
+	std::string id;
+
+	std::string userId;
+	std::string postId;
+};
